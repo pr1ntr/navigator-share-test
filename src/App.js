778493a91d95e442edf75e17a3531a6399c93b15
Pavel Zagoskin <d4rklit3@gmail.com>
2021-02-17 11:28:57 -0800

    test native share api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
+import { useCallback } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const IMAGE_URL = '/testshare.jpg'
+
+const downloadImageToFile = async () => {
+  const download = await fetch(IMAGE_URL)
+
+  const imageBlob = await download.blob()
+  const file = new File([imageBlob], 'testshare.jpg', { type: imageBlob.type })
+  console.log(imageBlob.type)
+
+  return file
+
+}
+
+
 function App() {
+
+
+
+
+
+  const onShare = useCallback(async () =>{
+    const blob = await downloadImageToFile()
+
+    const files = Object.freeze([blob])
+    if(navigator.canShare && navigator.canShare({ files })) {
+      console.log('share?')
+      navigator.share({
+        files,
+        title: 'picture'
+      })
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onShare}>Share</button>
       </header>
     </div>
   );
